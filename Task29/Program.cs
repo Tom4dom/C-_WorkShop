3110709bc5c0487d33934d6d790d4ff4329b1b34
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2023-01-18 22:37:08 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт
+// массив из 8 элементов, заполненный
+// псевдослучайными числами и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] CreateArray(int size)
+{
+    int[] nameOfArray = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < nameOfArray.Length; i++)
+    {
+        nameOfArray[i] = rnd.Next(0, 100);
+    }
+    return nameOfArray;
+}
+
+
+
+void PrintArray(int[] arrayAsParameter)
+{
+    for (int i = 0; i < arrayAsParameter.Length; i++)
+    {
+        Console.Write($"{arrayAsParameter[i]} ");
+    }
+}
+
+
+int[] arr = CreateArray(16);
+PrintArray(arr);
+
