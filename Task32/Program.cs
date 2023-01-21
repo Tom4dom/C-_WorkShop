b3a73119136a9f93eed13112c474c280aa444147
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2023-01-21 14:45:39 +0300

    Task32_lecture_code_has_been_added

--- Task32/Program.cs
+++ Task32/Program.cs
@@ -3,7 +3,7 @@
 // соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
 
-
+//========================================================MyVision===============================================
 
 int[] ArrayCreator(int size, int min, int max)
 {
@@ -51,6 +51,56 @@ ArrayPrinter(array);
 
 
 
+
+
+
+
+
+
+
+//===============================ClassVision==================================================
+
+
+// / Задача 32: Напишите программу замена элементов
+// // массива: положительные элементы замените на
+// // соответствующие отрицательные, и наоборот.
+// // [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+// int[] array = new int[size];
+// Random rnd = new Random();
+// for (int i = 0; i < array.Length; i++)
+// {
+// array[i] = rnd.Next(min, max + 1);
+// }
+// return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+// System.Console.Write("[");
+// for (int i = 0; i < array.Length; i++)
+// {
+// if (i < array.Length - 1) System.Console.Write(array[i] + ",");
+// else System.Console.Write(array[i]);
+// }
+// System.Console.WriteLine("]");
+// }
+
+// void ChangeOverNumb(int[] array)
+// {
+// for (int i = 0; i < array.Length; i++)
+// {
+// array[i] = -array[i];
+// }
+// }
+
+// int[] create = CreateArrayRndInt(4, -10, 10);
+
+// PrintArray(create);
+// ChangeOverNumb(create);
+// PrintArray(create);
 
 
 
