06308ba1ed715ea3ed73f96bd36c0d71508c3eea
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2023-01-22 22:11:46 +0300

    Task36_homework

---
+++ Task36/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 36: 
+// 1. Задайте одномерный массив, заполненный случайными числами. 
+// 2. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+int[] CreatArray(int arraySize, int arrayMinValue, int arrayMaxValue)//Метод генерации массива со случайными элементами
+{
+    int[] resArray = new int[arraySize];
+    Random rnd = new Random();
+    for (int i = 0; i < resArray.Length; i++)
+    {
+        resArray[i] = rnd.Next(arrayMinValue, arrayMaxValue + 1);
+    }
+    return resArray;
+}
+
+
+string ArrayToString(int[] array)//Метод преобразования массива в строку
+{
+    string result = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result = result + $" {array[i]}";
+        result = i < (array.Length - 1) ? result = result + "," : result;//для корректности вывода запятых
+    }
+    return result + " ]";
+}
+
+int EvenNumberCounter(int[] arr)
+{   
+    int counter = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            counter = counter + arr[i];
+        }
+    }
+    return counter;
+}
+
+int arraySize = 7, arrayMinValue = 1, arrayMaxValue = 10;
+
+int[] newArray = CreatArray(arraySize, arrayMinValue, arrayMaxValue); //генерируем массив с указаными параметры
+string strArray = ArrayToString(newArray);// переводим строку в массив для дальнейшего вывода на экран
+int count = EvenNumberCounter(newArray);
+
+Console.WriteLine($"{strArray} -> {count} ");
