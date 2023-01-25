7b873e8110739aa31fa736437541d182aa09942b
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2023-01-25 18:45:50 +0300

    Task44

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -33,6 +33,11 @@ void MightOrMightNotExist(int a, int b, int c)
     }
 }
 
+bool MightOrMightNotExistBOOOOOOOL(int a, int b, int c)
+{
+  return (a < b+c && b < a+c && c < a+b);
+}
+
 
 
 int numA = UsrInputNums("Enter integer A: ");
@@ -43,6 +48,8 @@ Console.WriteLine(numB);
 Console.WriteLine(numC);
 
 MightOrMightNotExist(numA, numB, numC);
+
+Console.WriteLine(MightOrMightNotExistBOOOOOOOL(numA, numB, numC) ? $"A triangle with sides {numA}, {numB}, {numC} can exist" : "Can't");
 
 
 
@@ -63,3 +70,38 @@ MightOrMightNotExist(numA, numB, numC);
 // else System.Console.WriteLine("Нет");
 
 // 
+
+
+
+
+
+
+
+
+
+// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+
+// int side1 = NumberInput("Please, input your number 1");
+// int side2 = NumberInput("Please, input your number 2");
+// int side3 = NumberInput("Please, input your number 3");
+// Console.WriteLine(ExistTriangle(side1, side2, side3) ? "Triangle Exist" : "Triangle Does not exist");
+
+// int NumberInput(string text) // Method fot Number input
+// {
+// Console.WriteLine(text);
+// int numberinput = Convert.ToInt32(Console.ReadLine());
+// return numberinput;
+// }
+
+// bool ExistTriangle(int s1, int s2, int s3)
+// {
+// return s1 < s2 + s3 && s2 < s1 + s3 && s3 < s2 + s1;
+// }
+
+
+
---
+++ Task44/Program.cs
@@ -0,0 +1,93 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+
+int InsertDigit(string text)
+{
+    Console.Write(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[] FibonacciWithoutRecursion(int num)
+{
+    int[] array = new int[num];
+    array[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        array[i] = array[i-1] + array[i-2];   
+    }
+    return array;
+}
+
+void ArrayPrinter(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + " ");
+        else Console.Write(array[i]);
+    }
+
+    Console.Write("]");
+}
+
+int userNumber = InsertDigit("Insert a number: ");
+int[] fibonumbers = FibonacciWithoutRecursion(userNumber);
+ArrayPrinter(fibonumbers);
+
+
+
+
+
+
+///==========================================================================================
+
+// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int number = NumberInput("Please, input your number: ");
+int[]arr = FibArray(number);
+PrintArray(arr);
+
+int NumberInput(string text) // Method fot Number input
+{
+Console.Write(text);
+int numberinput = Convert.ToInt32(Console.ReadLine());
+return numberinput;
+}
+
+int[]FibArray(int count)
+{
+int[] result = new int[count];
+result[1] = 1;
+for (int i = 2; i < count; i++)
+{
+result[i] = result[i-1]+result[i-2];
+}
+return result;
+}
+
+// Печать массива int
+void PrintArray(int[] array)
+{
+System.Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+if (i < array.Length - 1) System.Console.Write(array[i] + ",");
+else System.Console.Write(array[i]);
+}
+System.Console.WriteLine("]");
+}
+
+//================================================================================================================
+
