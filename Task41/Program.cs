0584f035260a3f8ee3fa09e93b876acf83be19a6
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2023-01-25 22:05:43 +0300

    Task41_homework

---
+++ Task41/Program.cs
@@ -0,0 +1,53 @@
+﻿
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+int[] UserInputMethod()
+{
+    Console.Write("Enter intergers separated by space (for example: 5 3 9 -3 4 5): ");
+    string line = Console.ReadLine();
+    string[] splittedStringArray = line.Split(' ');
+    int[] arr = new int[splittedStringArray.Length];
+
+
+    for (int i = 0; i < splittedStringArray.Length; i++)
+    {
+        int temp = int.Parse(splittedStringArray[i]);
+        arr[i] = temp;
+    }
+    return arr;
+}
+
+void ArrayPrinter(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + "; ");
+        else Console.Write(array[i]);
+    }
+
+    Console.Write("]");
+}
+
+int Counter(int[] collection)
+{
+    int counter = 0;
+    for (int i = 0; i < collection.Length; i++)
+    {
+        if (collection[i] > 0)
+        {
+            counter = counter + 1;
+        }
+    }
+    return counter;
+}
+
+int[] numbers = UserInputMethod();
+ArrayPrinter(numbers);
+int count = Counter(numbers);
+Console.WriteLine($" -> {count} number/numbers greater than 0");
--- Task45/Program.cs
+++ Task45/Program.cs
@@ -29,13 +29,13 @@ void ArrayPrinter(int[] array)
     Console.Write("]");
 }
 
-int[] ArrayMirrorer(int[] collection)
+int[] ArrayCopier(int[] collection)
 {
     int size = collection.Length;
     int[] array = new int[size];
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = collection[i]+1;
+        array[i] = collection[i];
         
     }
     return array;
@@ -45,8 +45,8 @@ int arraySize = 7, arrayMinValue = 1, arrayMaxValue = 9;
 int[] array = RandomNumbersArrayCreator(arraySize, arrayMinValue, arrayMaxValue);
 ArrayPrinter(array);
 Console.WriteLine();
-int[] mirroredArray = ArrayMirrorer(array);
-ArrayPrinter(mirroredArray);
+int[] copyOfArray = ArrayCopier(array);
+ArrayPrinter(copyOfArray);
 Console.WriteLine();
 
 
@@ -56,48 +56,48 @@ Console.WriteLine();
 
 
 
-//=========================================================================================
-// Задача 45: Напишите программу, которая будет создавать
-// копию заданного массива с помощью поэлементного
-// копирования.
+// //=========================================================================================
+// // Задача 45: Напишите программу, которая будет создавать
+// // копию заданного массива с помощью поэлементного
+// // копирования.
 
-int[] arr1 = CreateArrayRndInt(7, 1, 9);
-int[] arr2 = CopyArray(arr1);
-PrintArray(arr1);
-PrintArray(arr2);
+// int[] arr1 = CreateArrayRndInt(7, 1, 9);
+// int[] arr2 = CopyArray(arr1);
+// PrintArray(arr1);
+// PrintArray(arr2);
 
-// Создание рандомного массива int
-int[] CreateArrayRndInt(int size, int min, int max)
-{
-int[] array = new int[size];
-Random rnd = new Random();
-for (int i = 0; i < array.Length; i++)
-{
-array[i] = rnd.Next(min, max + 1);
-}
-return array;
-}
+// // Создание рандомного массива int
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+// int[] array = new int[size];
+// Random rnd = new Random();
+// for (int i = 0; i < array.Length; i++)
+// {
+// array[i] = rnd.Next(min, max + 1);
+// }
+// return array;
+// }
 
-// Печать массива int
-void PrintArray(int[] array)
-{
-System.Console.Write("[");
-for (int i = 0; i < array.Length; i++)
-{
-if (i < array.Length - 1) System.Console.Write(array[i] + ",");
-else System.Console.Write(array[i]);
-}
-System.Console.WriteLine("]");
-}
+// // Печать массива int
+// void PrintArray(int[] array)
+// {
+// System.Console.Write("[");
+// for (int i = 0; i < array.Length; i++)
+// {
+// if (i < array.Length - 1) System.Console.Write(array[i] + ",");
+// else System.Console.Write(array[i]);
+// }
+// System.Console.WriteLine("]");
+// }
 
-// Копия массива int
-int[] CopyArray(int[] input)
-{
-int[] result = new int[input.Length];
-for (int i = 0; i < input.Length; i++)
-{
-result[i] = input[i];
-}
-return result;
-}
+// // Копия массива int
+// int[] CopyArray(int[] input)
+// {
+// int[] result = new int[input.Length];
+// for (int i = 0; i < input.Length; i++)
+// {
+// result[i] = input[i];
+// }
+// return result;
+// }
 
