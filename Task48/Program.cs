4663567c66833e5f0716fca40f6266f6be762688
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2023-01-27 13:40:50 +0300

    Task48

---
+++ Task48/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 48: 
+
+// Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+
+int[,] TwoDementionalArrayCreator(int sizeRows, int sizeColumns, int min, int max)
+{
+    int[,] matrix = new int[sizeRows, sizeColumns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void ArrayPrinter(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] AmnEqualsMPlusN(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+
+int rows = 13;
+int columns = 21;
+int min = 0;
+int max = 199;
+int[,] myTwoDemArray = TwoDementionalArrayCreator(rows, columns, min, max);
+ArrayPrinter(myTwoDemArray);
+AmnEqualsMPlusN(myTwoDemArray);
+Console.WriteLine();
+ArrayPrinter(myTwoDemArray);
+
+
+
