0e4761fc3db69dac9629fade5b86b13e6667096b
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2023-02-01 12:46:16 +0300

    Task57_understood

--- Task57/Program.cs
+++ Task57/Program.cs
@@ -69,6 +69,7 @@ int[] MatrixToArray(int[,] matrix)
             index++;
         }
     }
+    Console.WriteLine();
     OneDemArrayPrinter(array);
     Console.WriteLine();
     Array.Sort(array);
@@ -77,15 +78,48 @@ int[] MatrixToArray(int[,] matrix)
 
 
 
-int rows = 4;
-int columns = 4;
-int min = 0;
-int max = 9;
+
+
+int[,] ElementFrequencyDictionaryCreator(int[] array)
+{
+    int[,] elementFreqTable = new int[array.Length, 2];
+    int minValue = array[0];
+    elementFreqTable[0, 0] = minValue;
+    elementFreqTable[0, 1] = 1;
+    int row = 0;
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] == minValue)
+        {
+            elementFreqTable[row, 1]++;
+        }
+        else
+        {
+            row++;
+            elementFreqTable[row, 0] = array[i];
+            elementFreqTable[row, 1]++;
+            minValue = array[i];
+        }
+    }
+    return elementFreqTable;
+}
+
+
+int rows = 3;
+int columns = 3;
+int min = -10;
+int max = 10;
 int[,] myTwoDemArray = TwoDementionalArrayCreator(rows, columns, min, max);
 ArrayPrinter(myTwoDemArray);
 int[] result = MatrixToArray(myTwoDemArray);
 Console.WriteLine();
 OneDemArrayPrinter(result);
+int[,] dictionaryMatrix = ElementFrequencyDictionaryCreator(result);
+Console.WriteLine();
+Console.WriteLine();
+ArrayPrinter(dictionaryMatrix);
+
 
 
 
@@ -103,127 +137,127 @@ OneDemArrayPrinter(result);
 // входных данных.
 
 
-int[,] CreatMatrixRndIntArray(int rowSize, int colSize, int minValue, int maxValue)//Метод генерации массива со случайными элементами
-{
-    int[,] resArray = new int[rowSize, colSize];
-    Random rnd = new Random();
+// int[,] CreatMatrixRndIntArray(int rowSize, int colSize, int minValue, int maxValue)//Метод генерации массива со случайными элементами
+// {
+//     int[,] resArray = new int[rowSize, colSize];
+//     Random rnd = new Random();
 
-    for (int i = 0; i < resArray.GetLength(0); i++)
-        for (int j = 0; j < resArray.GetLength(1); j++)
-            resArray[i, j] = rnd.Next(minValue, maxValue + 1);
+//     for (int i = 0; i < resArray.GetLength(0); i++)
+//         for (int j = 0; j < resArray.GetLength(1); j++)
+//             resArray[i, j] = rnd.Next(minValue, maxValue + 1);
 
-    return resArray;
-}
-
-
-string ArrayToString(int[,] array)//Метод преобразования массива в строку
-{
-    string result = "";
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        result += "[";
-
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            result = result + $" {array[i, j],3}";
-            result = j < (array.GetLength(1) - 1) ? result + "," : result;//для корректности вывода запятых    
-        }
-
-        result += "]\n";
-    }
-    return result;
-}
-
-string ArrayToString2(int[,] array)//Метод преобразования массива в строку
-{
-    string result = "";
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        if (array[i, 1] == 0) break;//Если количество элементов равно нулю мы дошли до низу массива
-        result += "[";
-
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            result = result + $" {array[i, j],3}";
-            result = j < (array.GetLength(1) - 1) ? result + "," : result;//для корректности вывода запятых    
-        }
-
-        result += "]\n";
-    }
-    return result;
-}
+//     return resArray;
+// }
 
 
-void PrintArray(int[] array)
-{
-    Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (i < array.Length - 1) Console.Write(array[i] + ", ");
-        else Console.WriteLine(array[i] + "]");
-    }
-}
+// string ArrayToString(int[,] array)//Метод преобразования массива в строку
+// {
+//     string result = "";
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         result += "[";
 
-int[] MatrixToArray(int[,] matrix)
-{
-    int[] array = new int[matrix.Length];
-    int k = 0;
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            array[k] = matrix[i, j];
-            k++;
-        }
-    }
-    return array;
-}
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             result = result + $" {array[i, j],3}";
+//             result = j < (array.GetLength(1) - 1) ? result + "," : result;//для корректности вывода запятых    
+//         }
 
-int[,] CreateDictionaryFromArray(int[] array)
-{
-    int[,] matrixArray = new int[array.Length, 2];
-    int currValue = array[0];
-    matrixArray[0, 0] = currValue;
-    matrixArray[0, 1] = 1;
-    int cnt = 0;
+//         result += "]\n";
+//     }
+//     return result;
+// }
 
-    for (int i = 1; i < array.Length; i++)
-    {
-        if (array[i] == currValue)
-        {
-            matrixArray[cnt, 1]++;
-        }
-        else
-        {
-            cnt++;
-            matrixArray[cnt, 0] = array[i];
-            matrixArray[cnt, 1]++;
-            currValue = array[i];
-        }
-    }
-    return matrixArray;
-}
+// string ArrayToString2(int[,] array)//Метод преобразования массива в строку
+// {
+//     string result = "";
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         if (array[i, 1] == 0) break;//Если количество элементов равно нулю мы дошли до низу массива
+//         result += "[";
+
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             result = result + $" {array[i, j],3}";
+//             result = j < (array.GetLength(1) - 1) ? result + "," : result;//для корректности вывода запятых    
+//         }
+
+//         result += "]\n";
+//     }
+//     return result;
+// }
 
 
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i < array.Length - 1) Console.Write(array[i] + ", ");
+//         else Console.WriteLine(array[i] + "]");
+//     }
+// }
 
-//Параметры для генерации двумерного массива типа int.
-int matrixRowSize = 3,
-    matrixColSize = 3,
-    matrixMinValue = -10,
-    matrixMaxValue = 10;
+// int[] MatrixToArray(int[,] matrix)
+// {
+//     int[] array = new int[matrix.Length];
+//     int k = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             array[k] = matrix[i, j];
+//             k++;
+//         }
+//     }
+//     return array;
+// }
 
-int[,] rndMatrixInt = CreatMatrixRndIntArray(matrixRowSize, matrixColSize, matrixMinValue, matrixMaxValue);
+// int[,] CreateDictionaryFromArray(int[] array)
+// {
+//     int[,] matrixArray = new int[array.Length, 2];
+//     int currValue = array[0];
+//     matrixArray[0, 0] = currValue;
+//     matrixArray[0, 1] = 1;
+//     int cnt = 0;
 
-Console.WriteLine(ArrayToString(rndMatrixInt));
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//         if (array[i] == currValue)
+//         {
+//             matrixArray[cnt, 1]++;
+//         }
+//         else
+//         {
+//             cnt++;
+//             matrixArray[cnt, 0] = array[i];
+//             matrixArray[cnt, 1]++;
+//             currValue = array[i];
+//         }
+//     }
+//     return matrixArray;
+// }
 
-int[] tmpArray = MatrixToArray(rndMatrixInt);
-Array.Sort(tmpArray);
 
-PrintArray(tmpArray);
 
-int[,] dictionaryMatrix = CreateDictionaryFromArray(tmpArray);
+// //Параметры для генерации двумерного массива типа int.
+// int matrixRowSize = 3,
+//     matrixColSize = 3,
+//     matrixMinValue = -10,
+//     matrixMaxValue = 10;
 
-Console.WriteLine(ArrayToString2(dictionaryMatrix));
+// int[,] rndMatrixInt = CreatMatrixRndIntArray(matrixRowSize, matrixColSize, matrixMinValue, matrixMaxValue);
+
+// Console.WriteLine(ArrayToString(rndMatrixInt));
+
+// int[] tmpArray = MatrixToArray(rndMatrixInt);
+// Array.Sort(tmpArray);
+
+// PrintArray(tmpArray);
+
+// int[,] dictionaryMatrix = CreateDictionaryFromArray(tmpArray);
+
+// Console.WriteLine(ArrayToString2(dictionaryMatrix));
 
 
 
@@ -235,23 +269,23 @@ Console.WriteLine(ArrayToString2(dictionaryMatrix));
 //=================================================================================================
 
 
-void CountDictionary(int[] arr)
-{
-    int count = 1;
-    int num = arr[0];
+// void CountDictionary(int[] arr)
+// {
+//     int count = 1;
+//     int num = arr[0];
 
-    for (int i = 1; i < arr.Length; i++)
-    {
-        if (arr[i] == num)
-            count++;
-        else
-        {
-            Console.WriteLine($"Quantity of numbers {num} = {count} ");
-            count = 1;
-			num = arr[i];
-		}
-	}
-	Console.WriteLine($"Quantity of numbers {num} = {count} ");
-}
+//     for (int i = 1; i < arr.Length; i++)
+//     {
+//         if (arr[i] == num)
+//             count++;
+//         else
+//         {
+//             Console.WriteLine($"Quantity of numbers {num} = {count} ");
+//             count = 1;
+// 			num = arr[i];
+// 		}
+// 	}
+// 	Console.WriteLine($"Quantity of numbers {num} = {count} ");
+// }
 
 
